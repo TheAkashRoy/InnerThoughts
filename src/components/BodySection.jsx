3bd0029f2836288b39c83673974072aa0273a820
Akash Roy <9akash.roy@gmail.com>
2024-01-20 13:14:56 +0530

    body spacing fixed

--- src/components/BodySection.jsx
+++ src/components/BodySection.jsx
@@ -5,12 +5,12 @@ import Button2 from "./Button2";
 
 function BodySection() {
   return (
-    <div className="flex flex-col-reverse md:flex-row justify-between py-14 gap-20 lg:gap-0">
-      <section className="sm:w-[700px] lg:w-[450px] flex flex-col gap-12">
-        <h1 className="text-5xl md:text-6xl font-bold leading-relaxed dark:text-white">
+    <div className="flex flex-col-reverse md:flex-row justify-between py-7 gap-20 lg:gap-0">
+      <section className="sm:w-[700px] lg:w-[450px] flex flex-col ">
+        <h1 className="text-5xl md:text-6xl font-bold leading-relaxed dark:text-white my-5">
           Welcome to Inner Thoughts
         </h1>
-        <div className="flex gap-7 items-center">
+        <div className="flex gap-7 items-center mt-5">
           <Button1 />
           {/* <Button2 /> */}
         </div>
