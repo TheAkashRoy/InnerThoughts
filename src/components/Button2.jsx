cbf2fa57b618887290efbc1f9c677c1ba1989ab1
Subhojit666 <subhojitghosh666@gmail.com>
2024-01-20 15:32:13 +0530

    button 2 updated

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -6,7 +6,7 @@ function Button2() {
     <Link to="/activity">
       <button className="uppercase bg-[#d08b2a] rounded-md hover:bg-[#d0722a]/80 
       transition duration-200 text-2xl dark:text-white font-semibold  py-4 px-8  w-full mb-4">
-        Activities
+        Let's do some activities
       </button>
     </Link>
   );
