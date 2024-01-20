72feb6e7a692708af44644526d68b07ba155d417
Akash Roy <9akash.roy@gmail.com>
2024-01-20 16:23:53 +0530

    distract page css fix

--- src/pages/Distract.jsx
+++ src/pages/Distract.jsx
@@ -7,27 +7,27 @@ function Distract() {
       <div className="bg-[rgba(255, 255, 255, 0.8)] dark:bg-[rgba(17, 23, 41, 0.8)] p-8 rounded-lg shadow-lg">
         {/* Add your five options here */}
         <Link to="/timer/Think of an animal for every letter of the alphabet.">
-        <button className="bg-[#d08b2a] text-white rounded-lg py-4 px-8 hover:bg-[#d0722a]/80 transition duration-200 w-full mb-4">
+        <button className="bg-[#59b02e] text-white rounded-lg py-4 px-8 hover:bg-[#95d177]/80 transition duration-200 w-full mb-4">
         Think of an animal for every letter of the alphabet.
         </button>
         </Link>
         <Link to="/timer/Can you name atleast Fifteen of your favourite TV series?">
-        <button className="bg-[#d08b2a] text-white rounded-lg py-4 px-8 hover:bg-[#d0722a]/80 transition duration-200 w-full mb-4">
+        <button className="bg-[#59b02e] text-white rounded-lg py-4 px-8 hover:bg-[#95d177]/80 transition duration-200 w-full mb-4">
         Can you name atleast Fifteen of your favourite TV series?
         </button>
         </Link>
         <Link to="/timer/Which superheroes would you like to have as your friends and why?">
-        <button className="bg-[#d08b2a] text-white rounded-lg py-4 px-8 hover:bg-[#d0722a]/80 transition duration-200 w-full mb-4">
+        <button className="bg-[#59b02e] text-white rounded-lg py-4 px-8 hover:bg-[#95d177]/80 transition duration-200 w-full mb-4">
         Which superheroes would you like to have as your friends and why?
         </button>
         </Link>
         <Link to="/timer/Count everything green that you see.">
-        <button className="bg-[#d08b2a] text-white rounded-lg py-4 px-8 hover:bg-[#d0722a]/80 transition duration-200 w-full mb-4">
+        <button className="bg-[#59b02e] text-white rounded-lg py-4 px-8 hover:bg-[#95d177]/80 transition duration-200 w-full mb-4">
         Count everything green that you see.
         </button>
         </Link>
         <Link to="/timer/Roll a dice. Think of that many number of things that have made you feel good.">
-        <button className="bg-[#d08b2a] text-white rounded-lg py-4 px-8 hover:bg-[#d0722a]/80 transition duration-200 w-full">
+        <button className="bg-[#59b02e] text-white rounded-lg py-4 px-8 hover:bg-[#95d177]/80 transition duration-200 w-full">
         Roll a dice. Think of that many number of things that have made you feel good.
         </button>
         </Link>
